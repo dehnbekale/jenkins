7ce0e2d5b45297081da93e5bf7e1a25e6dad85ad
randy <randy.bekalemd@gmail.com>
2025-09-02 20:10:30 +0200

    commit nouvelle branche

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello Randy World!");
+        System.out.println("Hello Randy World! dans la nouvelle branche ");
     }
 }
 
