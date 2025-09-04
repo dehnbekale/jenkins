2e21aa535a56af6405806550abb9c61a24730509
randy <randy.bekalemd@gmail.com>
2025-09-04 22:01:06 +0200

    new commit

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 
 public class Main {
     public static void main (String[] args) {
-           System.out.println("Un jenkins extra! pour le build");
+           System.out.println("Un jenkins extra pour le build");
            System.out.println("Hello Randy World dans la nouvelle branche");
+	   int i;
+	   for(i=0; i<12; i++){
+
+	        System.out.println("on compte: " + i + " fois. ");
+           }
     }
 }
 
