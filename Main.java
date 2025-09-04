ead792c24739bbcabe0516cd2b5e61a1724937a8
randy <randy.bekalemd@gmail.com>
2025-09-04 13:58:12 +0200

    test git

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello Randy World! dans la nouvelle branche ");
+           System.out.println("Un jenkins extra!");
+           System.out.println("Hello Randy World dans la nouvelle branche");
     }
 }
 
